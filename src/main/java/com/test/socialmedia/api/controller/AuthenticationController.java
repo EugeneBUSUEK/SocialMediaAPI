9ebc9f43d01ae34d9b4d4825c286fe69cbe4ee89
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-08-17 00:18:36 +0300

    docs: add open api documentation and swagger ui

--- src/main/java/com/test/socialmedia/api/controller/AuthenticationController.java
+++ src/main/java/com/test/socialmedia/api/controller/AuthenticationController.java
@@ -3,7 +3,11 @@ package com.test.socialmedia.api.controller;
 import com.test.socialmedia.api.security.AuthenticationService;
 import com.test.socialmedia.model.request.AuthenticationRequest;
 import com.test.socialmedia.model.request.RegisterRequest;
+import com.test.socialmedia.model.response.AuthenticationResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,22 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-//@Tag(name = "authentication", description = "authentication resources")
+@Tag(name = "Authentication", description = "Authentication resources")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
-    //    @Operation(summary = "register user")
-    @PostMapping("/register")
-    public ResponseEntity<?> register(
+    @Operation(summary = "register user")
+    @PostMapping(value = "/register",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest registerRequest
     ) {
         return ResponseEntity.ok(authenticationService.register(registerRequest));
     }
 
-    //    @Operation(summary = "authenticate user")
-    @PostMapping("/authenticate")
-    public ResponseEntity<?> authenticate(
+    @Operation(summary = "authenticate user")
+    @PostMapping(value = "/authenticate",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest authenticationRequest
     ) {
         return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
