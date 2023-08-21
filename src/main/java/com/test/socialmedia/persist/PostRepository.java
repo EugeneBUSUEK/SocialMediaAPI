00c22e86dc5b2fe1ac5f2ae4e3d023d73425697d
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-08-21 06:17:05 +0300

    clean: clean up imports

--- src/main/java/com/test/socialmedia/persist/PostRepository.java
+++ src/main/java/com/test/socialmedia/persist/PostRepository.java
@@ -9,5 +9,6 @@ import java.util.Collection;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
     Page<PostEntity> findByUserIdIs(Long user_id, Pageable pageable);
+
     Page<PostEntity> findByUserIdIn(Collection<Long> user_id, Pageable pageable);
 }
